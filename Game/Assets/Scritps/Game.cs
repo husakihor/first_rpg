3ef53271f537154c8b19a8e91fc986abc1719335
Husak Ihor <ihor.husak@epita.fr>
2021-03-01 00:45:13 +0100

    Initial commit and first tests

---
+++ Game/Assets/Scritps/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Transform player;
+
+    void Start()
+    {
+        Instantiate(player, new Vector3(0, 1, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
