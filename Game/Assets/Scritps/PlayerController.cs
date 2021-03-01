786a0f1624c1d97c276d03bfc4282c98f5f38b14
Husak Ihor <ihor.husak@epita.fr>
2021-03-01 14:18:12 +0100

    Player Controller (axis movement and speed)
    Created a simple character controller that can move to the side and run if left shift is pressed.
    Need to implement the camera rotation

---
+++ Game/Assets/Scritps/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hMove = -Input.GetAxisRaw("Horizontal");
+        float vMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(vMove, 0.0f, hMove);
+        if (move != Vector3.zero)
+        {
+            transform.Translate(move * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
---
+++ Game/Assets/Scritps/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            GameObject.Find("Player(Clone)").GetComponent<PlayerController>().speed = 5.0f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            GameObject.Find("Player(Clone)").GetComponent<PlayerController>().speed = 2.0f;
+        }
+    }
+}
