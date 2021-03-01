510545eb4445e42e6665893a40f8f25543cb8c16
Husak Ihor <ihor.husak@epita.fr>
2021-03-01 15:39:52 +0100

    Added health to character (no death system yet)

--- Game/Assets/Scritps/PlayerStats.cs
+++ Game/Assets/Scritps/PlayerStats.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int health = 100;
+    public int maxHealth = 100;
+    public int stamina = 100;
+    public int maxStamina = 100;
+    public int strength;
 
     void Start()
     {
-        
+        health = maxHealth;
+        stamina = maxStamina;
     }
 
-    // Update is called once per frame
-    void Update()
+    void move()
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -21,6 +26,25 @@ public class PlayerStats : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             GameObject.Find("Player(Clone)").GetComponent<PlayerController>().speed = 2.0f;
+        }
+    }
+
+    void hit(int damage)
+    {
+        if (damage < 1)
+            return;
+        health -= damage;
+        if (health <= 0)
+            Debug.Log("Dead");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            hit(10);
+            Debug.Log(health);
         }
     }
 }
