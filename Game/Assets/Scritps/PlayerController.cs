08b295328d7aab94de0231d464b54aa215724448
Husak Ihor <ihor.husak@epita.fr>
2021-03-01 14:45:56 +0100

    player can jump now

--- Game/Assets/Scritps/PlayerController.cs
+++ Game/Assets/Scritps/PlayerController.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 2.0f;
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionEnter() 
+    {
+        isGrounded = true;    
     }
 
     // Update is called once per frame
@@ -22,6 +35,12 @@ public class PlayerController : MonoBehaviour
         if (move != Vector3.zero)
         {
             transform.Translate(move * speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
 }
