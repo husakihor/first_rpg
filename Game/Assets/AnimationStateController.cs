de3c7f6f51ac3943a388f97e2cfc2895d63ff8d4
Husak Ihor <ihor.husak@epita.fr>
2021-03-07 23:25:14 +0100

    animations
    Implemented handling of walking running idling and back running

---
+++ Game/Assets/AnimationStateController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    int isBackRunningHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forwardPressed = Input.GetKey("z");
+        bool backwardPressed = Input.GetKey("s");
+        bool runPressed = Input.GetKey("left shift");
+
+
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isBackRunning = animator.GetBool("isBackRunning");
+
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+        
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+
+        if (!isBackRunning && backwardPressed)
+        {
+            Debug.Log("BackRun");
+            animator.SetBool("isBackRunning", true);
+        }
+
+        if (isBackRunning && !backwardPressed)
+        {
+            Debug.Log("NoMoreBackRun");
+            animator.SetBool("isBackRunning", false);
+        }
+
+        
+    }
+}
